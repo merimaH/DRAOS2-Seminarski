70856cd5e26e675da51c7e34d3b512af638b0ffc
Pašalić Sedin <spasalic1@etf.unsa.ba>
2019-03-23 01:07:09 +0100

    PoredanoRedom

--- RoboticParkingSystem/Program.cs
+++ RoboticParkingSystem/Program.cs
@@ -35,7 +35,7 @@ namespace RoboticParkingSystem
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form7());
+            Application.Run(new Form1());
             //Random linija
         }
     }
